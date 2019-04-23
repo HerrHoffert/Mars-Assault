1c37970bd6bca62d130ecf1c11d921a1128804af
Josef Hoffert <josef.hoffert@gmail.com>
2019-04-23 17:41:34 +0200

    Add existing code

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX = null;
+    [SerializeField] Transform parent = null;
+    [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+            scoreBoard.ScoreHit(scorePerHit);
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hits = hits - 1;
+        // TODO consider hit fx/sx
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float timeToLoadScene = 2f;
+
+    private void Start()
+    {
+        Invoke("LoadFirstScene", timeToLoadScene);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
